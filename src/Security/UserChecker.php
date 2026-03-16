d9f78e153c125c5e141c07990485e87c74aa78f2
Angelo Roberti <sarcastic6@gmail.com>
2026-03-16 14:00:18 -0400

    chore: fix DoctrineBundle and Symfony Security deprecation warnings

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -6,6 +6,7 @@ namespace App\Security;
 
 use App\Entity\User;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -22,7 +23,7 @@ class UserChecker implements UserCheckerInterface
         }
     }
 
-    public function checkPostAuth(UserInterface $user): void
+    public function checkPostAuth(UserInterface $user, ?TokenInterface $token = null): void
     {
         // No post-auth checks needed
     }
